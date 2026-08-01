183b85f5f784985f9bbaff8ed5023b90776ddccd
ESI Ventures <esi.ventures.llc@gmail.com>
2026-07-31 23:38:56 -0400

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+  iglesia: "Comunidad Cristiana de Restauración",
+
+  pastor: "Carlos Sanabria",
+
+  email: "ccr.comunidadcristiana@gmail.com",
+
+  whatsapp: {
+    oracion: "59176561680",
+    consejeria: "59167131579"
+  },
+
+  youtube: {
+    canal: "@comunidadcristianaderestau6630",
+    api: "https://youtube-channel-api.valiasoluciones.workers.dev"
+  }
+};
